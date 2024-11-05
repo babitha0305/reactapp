2a492abe7707a07b80425174b99edc49a20a4307
babitha0305 <marpally.babithagoud@gmail.com>
2024-11-05 10:09:51 +0530

    newapplayout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import ButtonAppBar from './AppBar';
+import TemporaryDrawer from './drawer';
+import ToggleButtons from './toggle button';
+
+function App() {
+  return (
+    <div className="App">
+    <ButtonAppBar />
+    <TemporaryDrawer />
+    <ToggleButtons /> 
+    </div>
+  );
+}
+
+export default App;
